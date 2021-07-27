c4333d3df38bbf3c0f72f7cb96c973b4a0ff2db6
N-eeraj <neeraj7rajeena@gmail.com>
2021-07-27 23:46:07 +0530

    removed possibilities & added probability of dice & cards

--- probability_calc.py
+++ probability_calc.py
@@ -1,22 +1,15 @@
-def padded(string, num):
-    if len(string) < num:
-        return "0" * (num - len(string)) + string
-    return string
-
 event = int(input("Welcome\n1. Coin Toss\n2. Die Roll\n3. Card Draw\nSelect Option: "))
 
 if event == 1:
     possibility = ("Head", "Tail")
     coins = int(input("Enter number of coins: "))
-    print("\nPossible Outcomes: ")
-    for chances in range(2 ** coins):
-        for outcome in list(padded(str(bin(chances))[2:], coins)):
-            print(possibility[int(outcome)], end = " ")
-        print()
     print(f"Probability for any outcome: {1 / 2 ** coins * 100}%")
 elif event == 2:
-    dice = int("Enter number of dice thrown: ")
+    possibility = tuple(range(1, 7))
+    dice = int(input("Enter number of dice thrown: "))
+    print(f"Probability for any outcome: {1 / 6 ** dice * 100}%")
 elif event == 3:
-    cards = int("Enter number of cards picked: ")
+    cards = int(input("Enter number of cards picked: "))
+    print(f"Probability for any outcome: {1 / 52 ** cards * 100}%")
 else:
     print("Invalid Option")
